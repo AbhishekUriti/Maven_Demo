2c5dc3e34c7a2f39127f66b06fd2d7e20eb144de
Abhishek <abhishekuriti52067@gmail.com>
2026-02-23 15:40:37 +0530

    Second Commits Methods

---
+++ src/test/java/qedge/Demo.java
@@ -0,0 +1,16 @@
+package qedge;
+
+public class Demo {
+    public void add(){
+        System.out.println("Add");
+    }
+    public void sub(){
+        System.out.println("Sub");
+    }
+    public void mul(){
+        System.out.println("Mul");
+    }
+    public void div(){
+        System.out.println("Div");
+    }
+}
