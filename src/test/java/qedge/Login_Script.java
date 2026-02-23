a60bf3b7293b37d025626db32c9e886287e5189a
Abhishek <abhishekuriti52067@gmail.com>
2026-02-23 15:53:56 +0530

    New methods upadted

---
+++ src/test/java/qedge/Login_Script.java
@@ -0,0 +1,13 @@
+package qedge;
+
+public class Login_Script {
+    public void login(){
+        System.out.println("Login");
+    }
+    public void customer(){
+        System.out.println("Customer Login");
+    }
+    public void supplier(){
+        System.out.println("Login");
+    }
+}
