7b334d5530ecbfdfc5ec9d65c099be2d23955a08
Abhishek <abhishekuriti52067@gmail.com>
2026-02-23 16:25:19 +0530

    Switched Commit 1

---
+++ src/test/java/qedge/demo2.java
@@ -0,0 +1,19 @@
+package qedge;
+
+public class demo2 {
+    public void add(){
+        System.out.println("Add");
+    }
+    public void sub(){
+        System.out.println("Sub");
+    }
+    public void mul(){
+        System.out.println("Mul");
+    }
+    public void div(){
+        System.out.println("Div");
+    }
+    public void lcm(){
+        System.out.println("LCM");
+    }
+}
